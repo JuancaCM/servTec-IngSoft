818213ada0aae9bdfa96187e7ed7ec55b0ae49f6
pluengom <pablo.luengo@alu.ucm.cl>
2022-11-17 05:17:07 -0300

    OrdenTabla

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,58 +7,60 @@
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
             <a href='registroProcedimientos' class="btn btn-primary">Nueva orden</a>
         </div>
-        <table class="table mt-3">
-            <thead>
-                <tr>
-                    <th scope="col">OT #ID</th>
-                    <th scope="col">RUT</th>
-                    <th scope="col">Marca</th>
-                    <th scope="col">Modelo</th>
-                    <th scope="col">Valor</th>
-                    <th scope="col">Estado</th>
-                    <th scope="col">Acción</th>
-                    <th scope="col">Ticket</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($procedimientos as $p)
+        <div class="card mt-3 border-left-primary shadow">
+            <table class="table mt-3">
+                <thead>
                     <tr>
-                        <td>
-                            {{ $p->id }}
-                        </td>
-                        <td>
-                            {{ $p->equipo->cliente->rut }}
-                        </td>
-                        <td>
-                            {{ $p->equipo->marca }}
-                        </td>
-                        <td>
-                            {{ $p->equipo->modelo }}
-                        </td>
-                        <td>
-                            {{ $p->valor }}
-                        </td>
-                        <td>
-
-                            <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                {{ $p->estado->estado }}
-                            </button>
-                            <ul class="dropdown-menu">
-                                @foreach ($estados as $e)
-                                    <li><a class="dropdown-item" href="#">{{ $e->estado }}</a></li>
-                                @endforeach
-                            </ul>
-                        </td>
-                        <td>
-                            <a href='dashboard/{{ $p->id }}' class="btn btn-outline-warning">Detalle</a>
-                        </td>
-                        <td>
-                            <button type="button" class="btn btn-outline-dark">Ticket</button>
-                        </td>
+                        <th scope="col">OT #ID</th>
+                        <th scope="col">RUT</th>
+                        <th scope="col">Marca</th>
+                        <th scope="col">Modelo</th>
+                        <th scope="col">Valor</th>
+                        <th scope="col">Estado</th>
+                        <th scope="col">Acción</th>
+                        <th scope="col">Ticket</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($procedimientos as $p)
+                        <tr>
+                            <td class="align-middle">
+                                {{ $p->id }}
+                            </td>
+                            <td class="align-middle">
+                                {{ $p->equipo->cliente->rut }}
+                            </td>
+                            <td class="align-middle">
+                                {{ $p->equipo->marca }}
+                            </td>
+                            <td class="align-middle">
+                                {{ $p->equipo->modelo }}
+                            </td>
+                            <td class="align-middle">
+                                {{ $p->valor }}
+                            </td>
+                            <td class="align-middle">
+
+                                <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    {{ $p->estado->estado }}
+                                </button>
+                                <ul class="dropdown-menu">
+                                    @foreach ($estados as $e)
+                                        <li><a class="dropdown-item" href="#">{{ $e->estado }}</a></li>
+                                    @endforeach
+                                </ul>
+                            </td>
+                            <td class="align-middle">
+                                <a href='dashboard/{{ $p->id }}' class="btn btn-outline-warning">Detalle</a>
+                            </td>
+                            <td class="align-middle">
+                                <button type="button" class="btn btn-outline-dark">Ticket</button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
