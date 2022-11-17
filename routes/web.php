f682ebc6dda81f204deeb23a39784add6134f53d
pluengom <pablo.luengo@alu.ucm.cl>
2022-11-17 00:27:20 -0300

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -3,30 +3,41 @@
 use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\authController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Middleware\Middleware;
+use App\Models\Procedimiento;
+use Illuminate\Http\Request;
+
+Route::middleware([Middleware::class])->group( function () {
+    Route::get('/registro', [ClienteController::class, 'formulario']);
+    Route::post('/registro', [ClienteController::class, 'guardar']);
+
+    Route::get('/registroProcedimientos', function () {
+        return view('clientes.registroProcedimientos');
+    });
+
+    Route::get('/procedimientos', function () {
+        return view('clientes.listaProcedimientos');
+    });
+
+    Route::get('/clientes', function () {
+        return view('clientes.listaClientes');
+    });
+
+    Route::get('/dashboard', [DashboardController::class,'getDashboard']);
+
+    Route::get('/logout', function (Request $req) {
+        $req->session()->flush();;
+        return redirect('/login');
+    });
+
+    Route::get('/dashboard/{id}', [DashboardController::class,'showProc']);
+
+});
 
 Route::get('/', function () {
     return view('base');
 });
 
-Route::get('/registro',[ClienteController::class, 'formulario']);
-Route::post('/registro',[ClienteController::class, 'guardar']);
-
-Route::get('/login',[authController::class,'login']);
-Route::post('/login',[authController::class,'loginUser']);
-
-Route::get('/registroProcedimientos', function () {
-    return view('clientes.registroProcedimientos');
-});
-
-Route::get('/procedimientos', function () {
-    return view('clientes.listaProcedimientos');
-});
-
-Route::get('/clientes', function () {
-    return view('clientes.listaClientes');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
+Route::get('/login', [authController::class, 'login']);
+Route::post('/login', [authController::class, 'loginUser']);
