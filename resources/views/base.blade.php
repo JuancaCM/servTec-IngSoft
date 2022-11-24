0af00e13d4fe8e3a32e85465741d5a7596df4a51
pluengom <pablo.luengo@alu.ucm.cl>
2022-11-24 02:16:09 -0300

    agregar elementos al sidebar

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -229,6 +229,14 @@
                         </li>
 
                         @if (session()->get('id') == 1)
+                            <h6
+                                class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+                                <i class="bi bi-house-gear-fill"></i>
+                                <span>Administración</span>
+                                <a class="link-secondary" href="#" aria-label="Add a new report">
+                                    <span data-feather="plus-circle" class="align-text-bottom"></span>
+                                </a>
+                            </h6>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">
                                     <span data-feather="users" class="align-text-bottom"></span>
@@ -241,27 +249,24 @@
                                 <a class="nav-link" href="#">
                                     <span data-feather="bar-chart-2" class="align-text-bottom"></span>
                                     <i class="bi bi-gear-fill"></i>
-                                    Administración
+                                    Configuración
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/listaUsuarios">
+                                    <span data-feather="layers" class="align-text-bottom"></span>
+                                    <i class="bi bi-person-lines-fill"></i>
+                                    Usuarios
                                 </a>
                             </li>
                         @endif
+
                     </ul>
-                        <!--
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                <span data-feather="layers" class="align-text-bottom"></span>
-                                Integrations
-                            </a>
-                        </li>
+                    <!--
 
 
-                    <h6
-                        class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-                        <span>Saved reports</span>
-                        <a class="link-secondary" href="#" aria-label="Add a new report">
-                            <span data-feather="plus-circle" class="align-text-bottom"></span>
-                        </a>
-                    </h6>
+
+
                     <ul class="nav flex-column mb-2">
                         <li class="nav-item">
                             <a class="nav-link" href="#">
