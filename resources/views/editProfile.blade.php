a926df92f956f4f6c4c4266e5e8be5010d943b3d
Greis <redblue1875@gmail.com>
2022-11-29 02:11:36 -0300

    editProfile-Password-BETA

--- resources/views/editProfile.blade.php
+++ resources/views/editProfile.blade.php
@@ -28,8 +28,29 @@
                     <div class="col-4">
                         <span><strong>Correo: </strong></span>
                     </div>
-                    <div class="col-8"><input type="text" class="form-control" id="nombre" name="correo"
+                    <div class="col-8"><input type="text" class="form-control" id="correo" name="correo"
                             value="{{ $usuario->correo }}"></div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-4">
+                        <span><strong>Contraseña anterior: </strong></span>
+                    </div>
+                    <div class="col-8"><input type="password" class="form-control" id="oldPassword" name="oldPassword"
+                            placeholder="Ingrese password"></div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-4">
+                        <span><strong>Contraseña nueva: </strong></span>
+                    </div>
+                    <div class="col-8"><input type="password" class="form-control" id="newPassword" name="newPassword"
+                            placeholder="Ingrese nueva password"></div>
+                </div>
+                <div class="row mb-2">
+                    <div class="col-4">
+                        <span><strong>Confirmar contraseña nueva: </strong></span>
+                    </div>
+                    <div class="col-8"><input type="password" class="form-control" id="newPassword2" name="newPassword2"
+                            placeholder="Confirmar nueva password"></div>
                 </div>
                 <div class="d-grid mt-3 gap-2 d-md-flex justify-content-md-end">
                     <a href='/dashboard' class="btn btn-primary">Atrás</a>
