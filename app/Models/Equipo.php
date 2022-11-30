28dce16f1cea619cc33e01e10c99c294655c2061
Greis <redblue1875@gmail.com>
2022-11-30 18:19:28 -0300

    softdeleteNoUsados

--- app/Models/Equipo.php
+++ app/Models/Equipo.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Equipo extends Model
 {
-
+    use SoftDeletes;
     use HasFactory;
+
     function cliente(){
-        return $this->belongsTo(Cliente::class);
+        return $this->belongsTo(Cliente::class)->withTrashed();
     }
     function procedimiento(){
         return $this->belongsTo(Procedimiento::class);
--- app/Models/Procedimiento.php
+++ app/Models/Procedimiento.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Procedimiento extends Model
 {
+    use SoftDeletes;
     function equipo()
     {
         return $this->belongsTo(Equipo::class);
